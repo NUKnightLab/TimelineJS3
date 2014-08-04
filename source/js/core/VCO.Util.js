b46213f635c64a7e55532a496743b468b4b5db94
Joe Germuska <joe@germuska.com>
2014-08-04 11:01:21 -0500

    implement slugify function

--- source/js/core/VCO.Util.js
+++ source/js/core/VCO.Util.js
@@ -291,5 +291,35 @@ VCO.Util = {
 		
 		
 		return vars;
+	},
+	slugify: function(str) {
+		// borrowed from http://stackoverflow.com/a/5782563/102476
+		str = str.replace(/^\s+|\s+$/g, ''); // trim
+		str = str.toLowerCase();
+
+		// remove accents, swap ñ for n, etc
+		var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+		var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+		for (var i=0, l=from.length ; i<l ; i++) {
+		str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+		}
+
+		str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+		.replace(/\s+/g, '-') // collapse whitespace and replace by -
+		.replace(/-+/g, '-'); // collapse dashes
+
+		str = str.replace(/^([0-9])/,'_$1');
+		return str;
 	}
 };
+
+
+
+
+
+
+
+
+
+
+
