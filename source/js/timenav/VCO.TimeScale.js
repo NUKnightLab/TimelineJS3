7fcf0325c121a7d90444cdaec58c9a376648ff25
Joe Germuska <joe@germuska.com>
2014-08-06 10:23:33 -0500

    restore snake case

--- source/js/timenav/VCO.TimeScale.js
+++ source/js/timenav/VCO.TimeScale.js
@@ -6,27 +6,27 @@
 ================================================== */
 VCO.TimeScale = VCO.Class.extend({
     
-    initialize: function (slides, pixelWidth) {
-        if (pixelWidth == null) { pixelWidth = 0; };
+    initialize: function (slides, pixel_width) {
+        if (pixel_width == null) { pixel_width = 0; };
 		
-		this.pixelsPerMilli = 0;
+		this.pixels_per_milli = 0;
         this.slides = slides;
 		
         this.earliest = slides[0].date.data.date_obj.getTime();
         this.latest = slides[slides.length - 1].date.data.date_obj.getTime();
-        this.spanInMillis = this.latest - this.earliest;
-        this.average = (this.spanInMillis)/this.slides.length;
+        this.span_in_millis = this.latest - this.earliest;
+        this.average = (this.span_in_millis)/this.slides.length;
 
-        this.setPixelWidth(pixelWidth);
+        this.setPixelWidth(pixel_width);
     },
     
     setPixelWidth: function(width) {
-        this.pixelWidth = width;
-        this.pixelsPerMilli = this.pixelWidth / this.spanInMillis;
+        this.pixel_width = width;
+        this.pixels_per_milli = this.pixel_width / this.span_in_millis;
     },
 
-    getPosition: function(timeInMillis) {
-        return ( timeInMillis - this.earliest ) * this.pixelsPerMilli
+    getPosition: function(time_in_millis) {
+        return ( time_in_millis - this.earliest ) * this.pixels_per_milli
     }
     
 });
