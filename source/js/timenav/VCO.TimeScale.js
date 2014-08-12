9987a8c41b7f0c6c87825008aca1ce9a6fd1cb6a
Joe Germuska <joe@germuska.com>
2014-08-12 16:51:17 -0500

    making progress

--- source/js/timenav/VCO.TimeScale.js
+++ source/js/timenav/VCO.TimeScale.js
@@ -41,6 +41,10 @@ VCO.TimeScale = VCO.Class.extend({
         return ( time_in_millis - this._earliest ) * this._pixels_per_milli
     },
 
+    getPositionInfo: function(idx) {
+        return this._positions[idx];
+    },
+
     getPixelsPerTick: function() {
         return this._axis_helper.getPixelsPerTick(this._pixels_per_milli);
     },
@@ -79,25 +83,40 @@ VCO.TimeScale = VCO.Class.extend({
 
         for (var i = 0; i < slides.length; i++) {
             var pos_info = { start: this.getPosition(slides[i].start_date.getTime()) }
+            this._positions.push(pos_info);
             if (typeof(slides[i].end_date) != 'undefined') {
-                pos_info.end = this.getPosition(slides[i].end_date.getTime());
+                var end_pos = this.getPosition(slides[i].end_date.getTime());
+                pos_info.width = end_pos - pos_info.start;
+                if (pos_info.width > default_marker_width) {
+                    pos_info.end = pos_info.start + pos_info.width; 
+                } else {
+                    pos_info.end = pos_info.start + default_marker_width; 
+                }
             } else {
+                pos_info.width = default_marker_width;
                 pos_info.end = pos_info.start + default_marker_width;
             }
         };
 
-        for (var i = 0; i < lasts_in_rows.length; i++) {
-            if (pos_info.start > lasts_in_rows[i].end) {
-                pos_info.row = i;
-                lasts_in_rows[i] = pos_info;
-                break;
+        for (var i = 0; i < this._positions.length; i++) {
+            var pos_info = this._positions[i];
+            for (var j = 0; j < lasts_in_rows.length; j++) {
+                console.log("slide " + i + " new start: " + pos_info.start + " >? row " + j + " end: " + lasts_in_rows[j].end);
+                console.log("boolean:",(pos_info.start > lasts_in_rows[j].end))
+                if (pos_info.start > lasts_in_rows[j].end) {
+                    pos_info.row = j;
+                    lasts_in_rows[j] = pos_info;
+                    console.log("put slide " + i + " in row " + j)
+                    break;
+                }
+            };
+            if (typeof(pos_info.row) == 'undefined') {
+                console.log("need a new row for slide " + i);
+                pos_info.row = lasts_in_rows.length;
+                lasts_in_rows.push(pos_info);
             }
+
         };
-        if (!pos_info.row) {
-            pos_info.row = lasts_in_rows.length;
-            lasts_in_rows.push(pos_info);
-        }
-        this._positions.push(pos_info);
         return lasts_in_rows.length;
         
     }
