2d8426d94b09e48eea0bba712c7b6bedd1ec83e5
jywsn <jennifer.y.wilson@northwestern.edu>
2014-09-08 16:05:35 -0500

    Add non-codekit build config

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from os.path import abspath, basename, dirname, join
+import sys
+from fabric.api import env
+
+#
+# Project-specific settings, alter as needed
+#
+env.project_name = basename(dirname(__file__))
+env.django = False
+
+#
+# Add paths
+#
+def add_paths(*args):
+    """Make paths are in sys.path."""
+    for p in args:
+        if p not in sys.path:
+            sys.path.append(p)
+
+project_path = dirname(abspath(__file__))
+repos_path = dirname(project_path)
+
+add_paths(project_path, repos_path, join(project_path, 'website'))
+
+#
+# Import from fablib
+#
+from fablib import *
