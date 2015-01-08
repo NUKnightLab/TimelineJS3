f2ac851381d00ee4cf184b218d191fe093304e0c
Heather Billings <heatherjaybillings@gmail.com>
2015-01-08 15:51:01 -0600

    fix height on maps

--- build/js/timeline.js
+++ build/js/timeline.js
@@ -6948,26 +6948,25 @@ VCO.Media.Instagram = VCO.Media.extend({
 ================================================== */
 
 VCO.Media.Map = VCO.Media.extend({
-    
     includes: [VCO.Events],
-    
+
     _API_KEY: "AIzaSyB9dW8e_iRrATFa8g24qB6BDBGdkrLDZYI",
     /*  Load the media
     ================================================== */
     _loadMedia: function() {
-        
+
         // Loading Message
         this.loadingMessage();
-        
+
         // Create Dom element
         this._el.content_item   = VCO.Dom.create("div", "vco-media-item vco-media-map", this._el.content);
         this._el.content_container.className = "vco-media-content-container vco-media-content-container-text";
-        
+
         // Get Media ID (why?)
         this.media_id = this.data.url;
-        
+
         // API Call
-        
+
         this.mapframe = VCO.Dom.create("iframe", "", this._el.content_item);
         window.stash = this;
         this.mapframe.width       = "100%";
@@ -6978,16 +6977,16 @@ VCO.Media.Map = VCO.Media.extend({
         // After Loaded
         this.onLoaded();
     },
-    
-    updateMediaDisplay: function() {
-        
-    },
-    
-    _updateMediaDisplay: function() {
-        
-    }
 
-    
+    _updateMediaDisplay: function() {
+			if (this._state.loaded) {
+        console.log("el: ", this._el.content_item);
+        console.log("offsetWidth: ", this._el.content_item.offsetWidth);
+        var dimensions = VCO.Util.ratio.square({w:this._el.content_item.offsetWidth});
+        this._el.content_item.style.height = dimensions.h + "px";
+        console.log("final height: ", this._el.content_item.style.height);
+      }
+    }
 });
 
 
--- source/js/media/types/VCO.Media.Map.js
+++ source/js/media/types/VCO.Media.Map.js
@@ -2,26 +2,25 @@
 ================================================== */
 
 VCO.Media.Map = VCO.Media.extend({
-    
     includes: [VCO.Events],
-    
+
     _API_KEY: "AIzaSyB9dW8e_iRrATFa8g24qB6BDBGdkrLDZYI",
     /*  Load the media
     ================================================== */
     _loadMedia: function() {
-        
+
         // Loading Message
         this.loadingMessage();
-        
+
         // Create Dom element
         this._el.content_item   = VCO.Dom.create("div", "vco-media-item vco-media-map", this._el.content);
         this._el.content_container.className = "vco-media-content-container vco-media-content-container-text";
-        
+
         // Get Media ID (why?)
         this.media_id = this.data.url;
-        
+
         // API Call
-        
+
         this.mapframe = VCO.Dom.create("iframe", "", this._el.content_item);
         window.stash = this;
         this.mapframe.width       = "100%";
@@ -32,14 +31,11 @@ VCO.Media.Map = VCO.Media.extend({
         // After Loaded
         this.onLoaded();
     },
-    
-    updateMediaDisplay: function() {
-        
-    },
-    
-    _updateMediaDisplay: function() {
-        
-    }
 
-    
+    _updateMediaDisplay: function() {
+			if (this._state.loaded) {
+        var dimensions = VCO.Util.ratio.square({w:this._el.content_item.offsetWidth});
+        this._el.content_item.style.height = dimensions.h + "px";
+      }
+    }
 });
