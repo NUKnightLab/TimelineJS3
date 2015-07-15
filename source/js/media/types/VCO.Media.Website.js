1a01e77b34bfd7ba97677bc4cbf2affa6c8c07e5
Joe Germuska <joe@germuska.com>
2015-07-15 10:49:10 -0500

    if there is no embedly API key, create a card instead. At the moment, we still have a default API key, so this won't be seen, but it was tested. Basically WIP for #169

--- source/js/media/types/VCO.Media.Website.js
+++ source/js/media/types/VCO.Media.Website.js
@@ -16,18 +16,37 @@ VCO.Media.Website = VCO.Media.extend({
 		
 		// Get Media ID
 		this.media_id = this.data.url.replace(/.*?:\/\//g, "");
-		// API URL
-		api_url = "http://api.embed.ly/1/extract?key=" + this.options.api_key_embedly + "&url=" + this.media_id + "&callback=?";
-		
-		// API Call
-		VCO.getJSON(api_url, function(d) {
-			self.createMedia(d);
-		});
-		
-		
+
+		if (this.options.api_key_embedly) {
+			// API URL
+			api_url = "http://api.embed.ly/1/extract?key=" + this.options.api_key_embedly + "&url=" + this.media_id + "&callback=?";
+			
+			// API Call
+			VCO.getJSON(api_url, function(d) {
+				self.createMedia(d);
+			});
+		} else {
+			this.createCardContent();
+		}
 	},
 	
-	createMedia: function(d) {
+	createCardContent: function() {
+		(function(w, d){
+			var id='embedly-platform', n = 'script';
+			if (!d.getElementById(id)){
+			 w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
+			 var e = d.createElement(n); e.id = id; e.async=1;
+			 e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+			 var s = d.getElementsByTagName(n)[0];
+			 s.parentNode.insertBefore(e, s);
+			}
+		})(window, document);
+
+		var content = "<a href=\"" + this.data.url + "\" class=\"embedly-card\">" + this.data.url + "</a>";
+		this._setContent(content);
+
+	},
+	createMedia: function(d) { // this costs API credits...
 		var content = "";
 		
 		
@@ -42,6 +61,10 @@ VCO.Media.Website = VCO.Media.extend({
 		content		+=	"<span class='vco-media-website-description'>" + d.provider_name + "</span><br/>";
 		content		+=	"<p>" + d.description + "</p>";
 		
+		this._setContent(content);
+	},
+
+	_setContent: function(content) {
 		// Create Dom element
 		this._el.content_item	= VCO.Dom.create("div", "vco-media-item vco-media-website", this._el.content);
 		this._el.content_container.className = "vco-media-content-container vco-media-content-container-text";
@@ -49,6 +72,7 @@ VCO.Media.Website = VCO.Media.extend({
 		
 		// After Loaded
 		this.onLoaded();
+
 	},
 	
 	updateMediaDisplay: function() {
