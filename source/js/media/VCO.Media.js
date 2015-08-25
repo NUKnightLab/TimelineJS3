116ba59d0d32c12c9e416d9a6d0e8883e4d8d95d
Joe Germuska <joe@germuska.com>
2015-08-25 11:38:21 -0500

    linkify captions and credits

--- source/js/media/VCO.Media.js
+++ source/js/media/VCO.Media.js
@@ -5,11 +5,11 @@
 // TODO add link
 
 VCO.Media = VCO.Class.extend({
-	
+
 	includes: [VCO.Events, VCO.I18NMixins],
-	
+
 	_el: {},
-	
+
 	/*	Constructor
 	================================================== */
 	initialize: function(data, options, add_to_container) {
@@ -25,27 +25,27 @@ VCO.Media = VCO.Class.extend({
 			parent: {},
 			link: null
 		};
-		
+
 		// Player (If Needed)
 		this.player = null;
-		
+
 		// Timer (If Needed)
 		this.timer = null;
 		this.load_timer = null;
-		
+
 		// Message
 		this.message = null;
-		
+
 		// Media ID
 		this.media_id = null;
-		
+
 		// State
 		this._state = {
 			loaded: false,
 			show_meta: false,
 			media_loaded: false
 		};
-	
+
 		// Data
 		this.data = {
 			unique_id: 			null,
@@ -57,7 +57,7 @@ VCO.Media = VCO.Class.extend({
 			link: 				null,
 			link_target: 		null
 		};
-	
+
 		//Options
 		this.options = {
 			api_key_flickr: 		"f2cc870b4d233dd0a5bfe73fd0d64ef0",
@@ -66,32 +66,32 @@ VCO.Media = VCO.Class.extend({
 			credit_height: 			0,
 			caption_height: 		0
 		};
-	
+
 		this.animator = {};
-		
+
 		// Merge Data and Options
 		VCO.Util.mergeData(this.options, options);
 		VCO.Util.mergeData(this.data, data);
-		
+
 		this._el.container = VCO.Dom.create("div", "vco-media");
-		
+
 		if (this.data.unique_id) {
 			this._el.container.id = this.data.unique_id;
 		}
-		
-		
+
+
 		this._initLayout();
-		
+
 		if (add_to_container) {
 			add_to_container.appendChild(this._el.container);
 			this._el.parent = add_to_container;
 		};
-		
+
 	},
-	
+
 	loadMedia: function() {
 		var self = this;
-		
+
 		if (!this._state.loaded) {
 			try {
 				this.load_timer = setTimeout(function() {
@@ -103,14 +103,14 @@ VCO.Media = VCO.Class.extend({
 				trace("Error loading media for ", this._media);
 				trace(e);
 			}
-			
+
 			//this._state.loaded = true;
 		}
-		
-		
-		
+
+
+
 	},
-	
+
 	loadingMessage: function() {
 		this.message.updateMessage(this._('loading') + " " + this.options.media_name);
 	},
@@ -126,14 +126,14 @@ VCO.Media = VCO.Class.extend({
 
 	updateMediaDisplay: function(layout) {
 		if (this._state.loaded) {
-			
-			
+
+
 			if (VCO.Browser.mobile) {
 				this._el.content_item.style.maxHeight = (this.options.height/2) + "px";
 			} else {
 				this._el.content_item.style.maxHeight = this.options.height - this.options.credit_height - this.options.caption_height - 30 + "px";
 			}
-			
+
 			//this._el.content_item.style.maxWidth = this.options.width + "px";
 			this._el.container.style.maxWidth = this.options.width + "px";
 			// Fix for max-width issues in Firefox
@@ -142,11 +142,11 @@ VCO.Media = VCO.Class.extend({
 					//this._el.content_item.style.width = "100%";
 				}
 			}
-			
-			
-			
+
+
+
 			this._updateMediaDisplay(layout);
-			
+
 			if (this._state.media_loaded) {
 				if (this._el.credit) {
 					this._el.credit.style.width		= this._el.content_item.offsetWidth + "px";
@@ -155,16 +155,16 @@ VCO.Media = VCO.Class.extend({
 					this._el.caption.style.width		= this._el.content_item.offsetWidth + "px";
 				}
 			}
-			
+
 		}
 	},
-	
+
 	/*	Media Specific
 	================================================== */
 		_loadMedia: function() {
-		
+
 		},
-		
+
 		_updateMediaDisplay: function(l) {
 			//this._el.content_item.style.maxHeight = (this.options.height - this.options.credit_height - this.options.caption_height - 16) + "px";
 			if(VCO.Browser.firefox) {
@@ -172,40 +172,40 @@ VCO.Media = VCO.Class.extend({
 				this._el.content_item.style.width = "auto";
 			}
 		},
-		
+
 		_getMeta: function() {
-			
+
 		},
-	
+
 	/*	Public
 	================================================== */
 	show: function() {
-		
+
 	},
-	
+
 	hide: function() {
-		
+
 	},
-	
+
 	addTo: function(container) {
 		container.appendChild(this._el.container);
 		this.onAdd();
 	},
-	
+
 	removeFrom: function(container) {
 		container.removeChild(this._el.container);
 		this.onRemove();
 	},
-	
+
 	// Update Display
 	updateDisplay: function(w, h, l) {
 		this._updateDisplay(w, h, l);
 	},
-	
+
 	stopMedia: function() {
 		this._stopMedia();
 	},
-	
+
 	loadErrorDisplay: function(message) {
 		try {
 			this._el.content.removeChild(this._el.content_item);
@@ -214,7 +214,7 @@ VCO.Media = VCO.Class.extend({
 		}
 		this._el.content_item	= VCO.Dom.create("div", "vco-media-item vco-media-loaderror", this._el.content);
 		this._el.content_item.innerHTML = "<div class='vco-icon-" + this.options.media_type + "'></div><p>" + message + "</p>";
-		
+
 		// After Loaded
 		this.onLoaded(true);
 	},
@@ -232,7 +232,7 @@ VCO.Media = VCO.Class.extend({
 		}
 		this.updateDisplay();
 	},
-	
+
 	onMediaLoaded: function(e) {
 		this._state.media_loaded = true;
 		this.fire("media_loaded", this.data);
@@ -243,49 +243,49 @@ VCO.Media = VCO.Class.extend({
 			this._el.caption.style.width		= this._el.content_item.offsetWidth + "px";
 		}
 	},
-	
+
 	showMeta: function(credit, caption) {
 		this._state.show_meta = true;
 		// Credit
 		if (this.data.credit && this.data.credit != "") {
 			this._el.credit					= VCO.Dom.create("div", "vco-credit", this._el.content_container);
-			this._el.credit.innerHTML		= this.data.credit;
+			this._el.credit.innerHTML		= VCO.Util.linkify(this.data.credit);
 			this.options.credit_height 		= this._el.credit.offsetHeight;
-		} 
-		
+		}
+
 		// Caption
 		if (this.data.caption && this.data.caption != "") {
 			this._el.caption				= VCO.Dom.create("div", "vco-caption", this._el.content_container);
-			this._el.caption.innerHTML		= this.data.caption;
+			this._el.caption.innerHTML		= VCO.Util.linkify(this.data.caption);
 			this.options.caption_height 	= this._el.caption.offsetHeight;
-		} 
-		
+		}
+
 		if (!this.data.caption || !this.data.credit) {
 			this.getMeta();
 		}
-		
+
 	},
-	
+
 	getMeta: function() {
 		this._getMeta();
 	},
-	
+
 	updateMeta: function() {
 		if (!this.data.credit && this.data.credit_alternate) {
 			this._el.credit					= VCO.Dom.create("div", "vco-credit", this._el.content_container);
 			this._el.credit.innerHTML		= this.data.credit_alternate;
 			this.options.credit_height 		= this._el.credit.offsetHeight;
 		}
-		
+
 		if (!this.data.caption && this.data.caption_alternate) {
 			this._el.caption				= VCO.Dom.create("div", "vco-caption", this._el.content_container);
 			this._el.caption.innerHTML		= this.data.caption_alternate;
 			this.options.caption_height 	= this._el.caption.offsetHeight;
 		}
-		
+
 		this.updateDisplay();
 	},
-	
+
 	onAdd: function() {
 		this.fire("added", this.data);
 	},
@@ -293,21 +293,21 @@ VCO.Media = VCO.Class.extend({
 	onRemove: function() {
 		this.fire("removed", this.data);
 	},
-	
+
 	/*	Private Methods
 	================================================== */
 	_initLayout: function () {
-		
+
 		// Message
 		this.message = new VCO.Message({}, this.options);
 		this.message.addTo(this._el.container);
-		
+
 		// Create Layout
 		this._el.content_container = VCO.Dom.create("div", "vco-media-content-container", this._el.container);
-		
+
 		// Link
 		if (this.data.link && this.data.link != "") {
-			
+
 			this._el.link = VCO.Dom.create("a", "vco-media-link", this._el.content_container);
 			this._el.link.href = this.data.link;
 			if (this.data.link_target && this.data.link_target != "") {
@@ -315,44 +315,44 @@ VCO.Media = VCO.Class.extend({
 			} else {
 				this._el.link.target = "_blank";
 			}
-			
+
 			this._el.content = VCO.Dom.create("div", "vco-media-content", this._el.link);
-			
+
 		} else {
 			this._el.content = VCO.Dom.create("div", "vco-media-content", this._el.content_container);
 		}
-		
-		
+
+
 	},
-	
+
 	// Update Display
 	_updateDisplay: function(w, h, l) {
 		if (w) {
 			this.options.width = w;
-			
-		} 
+
+		}
 		//this._el.container.style.width = this.options.width + "px";
 		if (h) {
 			this.options.height = h;
 		}
-		
+
 		if (l) {
 			this.options.layout = l;
-		} 
-		
+		}
+
 		if (this._el.credit) {
 			this.options.credit_height 		= this._el.credit.offsetHeight;
 		}
 		if (this._el.caption) {
 			this.options.caption_height 	= this._el.caption.offsetHeight + 5;
 		}
-		
+
 		this.updateMediaDisplay(this.options.layout);
-		
+
 	},
-	
+
 	_stopMedia: function() {
-		
+
 	}
-	
+
 });
