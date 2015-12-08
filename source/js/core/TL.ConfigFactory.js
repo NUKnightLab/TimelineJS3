2a87770ade99f96105415542ca4d6e696a607af6
Ashley Wu <ashleywu2016@u.northwestern.edu>
2015-12-08 15:24:32 -0600

    improving parseGoogleSpreadsheetURL #311

--- source/js/core/TL.ConfigFactory.js
+++ source/js/core/TL.ConfigFactory.js
@@ -15,12 +15,14 @@
             worksheet: 0 // not really sure how to use this to get the feed for that sheet, so this is not ready except for first sheet right now
         }
         // key as url parameter (old-fashioned)
-        var pat = /\bkey=([-_A-Za-z0-9]+)&?/i;
-        if (url.match(pat)) {
-            parts.key = url.match(pat)[1];
+        var key_pat = /\bkey=([-_A-Za-z0-9]+)&?/i;
+        var url_pat = /docs.google.com\/spreadsheets(.*?)\/d\//; // fixing issue of URLs with u/0/d 
+
+        if (url.match(key_pat)) {
+            parts.key = url.match(key_pat)[1];
             // can we get a worksheet from this form?
-        } else if (url.match("docs.google.com/spreadsheets/d/")) {
-            var pos = url.indexOf("docs.google.com/spreadsheets/d/") + "docs.google.com/spreadsheets/d/".length;
+        } else if (url.match(url_pat)) {
+            var pos = url.search(url_pat) + url.match(url_pat)[0].length;
             var tail = url.substr(pos);
             parts.key = tail.split('/')[0]
             if (url.match(/\?gid=(\d+)/)) {
