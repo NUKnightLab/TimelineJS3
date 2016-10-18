84f806a989036d8cf4fc567cd2b2479f3a12c1c6
Scott Bradley <scottbradley.mail@gmail.com>
2016-10-18 09:04:05 -0500

    Adds clarifying note to hash-bookmarks option - does not work with iframe embeds

--- website/app.py
+++ website/app.py
@@ -16,10 +16,10 @@ import os
 
 # Add current directory to sys.path
 site_dir = os.path.dirname(os.path.abspath(__file__))
-         
+
 if site_dir not in sys.path:
     sys.path.append(site_dir)
-      
+
 # Set default FLASK_SETTINGS_MODULE for debug mode
 if not os.environ.get('FLASK_SETTINGS_MODULE', ''):
     os.environ['FLASK_SETTINGS_MODULE'] = 'core.settings.loc'
@@ -47,7 +47,7 @@ source_dir = os.path.join(settings.PROJECT_ROOT, 'source')
 def inject_static_url():
     """
     Inject the variables 'static_url' and 'STATIC_URL' into the templates to
-    avoid hard-coded paths to static files. Grab it from the environment 
+    avoid hard-coded paths to static files. Grab it from the environment
     variable STATIC_URL, or use the default. Never has a trailing slash.
     """
     static_url = settings.STATIC_URL or app.static_url_path
@@ -60,38 +60,38 @@ def catch_compiled(path):
     """
     Serve /compiled/... urls from the compiled directory
     """
-    return send_from_directory(compiled_dir, path)    
+    return send_from_directory(compiled_dir, path)
 
 @app.route('/build/<path:path>')
 def catch_build(path):
     """
     Serve /build/... urls from the build directory
     """
-    return send_from_directory(build_dir, path)    
+    return send_from_directory(build_dir, path)
 
 @app.route('/source/<path:path>')
 def catch_source(path):
     """
     Serve /source/... urls from the source directory
     """
-    return send_from_directory(source_dir, path)    
+    return send_from_directory(source_dir, path)
 
 @app.route('/')
 @app.route('/<path:path>')
 def catch_all(path='index.html', context=None):
     """Catch-all function which serves every URL."""
-    context = context or {}      
+    context = context or {}
     if not os.path.splitext(path)[1]:
         path = os.path.join(path, 'index.html')
     return render_template(path, **context)
-    
-        
+
+
 if __name__ == "__main__":
     import getopt
-    
+
     ssl_context = None
     port = 5000
-    
+
     try:
         opts, args = getopt.getopt(sys.argv[1:], "sp:", ["port="])
         for opt, arg in opts:
@@ -101,9 +101,9 @@ if __name__ == "__main__":
                 port = int(arg)
             else:
                 print 'Usage: app.py [-s]'
-                sys.exit(1)   
+                sys.exit(1)
     except getopt.GetoptError:
         print 'Usage: app.py [-s] [-p port]'
         sys.exit(1)
-        
+
     app.run(host='0.0.0.0', port=port, debug=True, ssl_context=ssl_context)
