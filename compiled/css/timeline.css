bce5176419451a2c4b761f60910f8b00ba002786
Scott Bradley <scottbradley.mail@gmail.com>
2017-03-31 10:36:14 -0500

    updates comiled

--- compiled/css/timeline.css
+++ compiled/css/timeline.css
@@ -1,5 +1,5 @@
 /*
-    TimelineJS - ver. 2017-01-12-20-45-53 - 2017-01-12
+    TimelineJS - ver. 2017-03-31-15-29-59 - 2017-03-31
     Copyright (c) 2012-2016 Northwestern University
     a project of the Northwestern University Knight Lab, originally created by Zach Wise
     https://github.com/NUKnightLab/TimelineJS3
