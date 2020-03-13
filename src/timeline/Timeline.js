6c9c40bb5e83eeb9c2ad5fe4eb0ff515109348b7
Joe Germuska <joe@germuska.com>
2020-03-13 15:22:48 -0500

    converted Load to class, now make other things use it.

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,14 +1,13 @@
 // simple test environment to make sure some things work
 import * as DOM from "../dom/DOM"
+// import { addClass } from "../dom/DOMUtil"
 import { hexToRgb, mergeData, isTrue } from "../core/Util";
 import { easeInOutQuint, easeOutStrong } from "../animation/Ease";
 import { Message } from "../ui/Message"
 import { Language } from "../language/Language"
+import { Loader } from "../core/Load"
 /*
     needed imports: 
-        TL.Ease.easeInOutQuint
-        TL.Ease.easeOutStrong
-        TL.DomUtil.addClass
         TL.Load.js
         TL.Language
         TL.Browser
@@ -21,11 +20,13 @@ import { Language } from "../language/Language"
 class Timeline {
     constructor(elem, data, options) {
       this.elem = DOM.get(elem)
-      this.options = options;
-      this.options.language = new Language(options);
-      this.elem.innerHTML = `<b>Hello World</b>`;
-      var msg_options = mergeData(options, { message_class: "tl-message-full" })
+      this.options = options || {};
+      this.options.language = new Language(this.options);
+      this.elem.innerHTML = `<b class='foobar'>Hello World</b>`;
+
+      var msg_options = mergeData(this.options, { message_class: "tl-message-full" })
       this.message = new Message({}, msg_options, this.elem);
+      this.loader = new Loader(window.document);
     }
 }
 
