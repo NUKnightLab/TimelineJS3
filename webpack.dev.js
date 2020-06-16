becca330b64c441b882ffea0889aa2c45ca5938a
Joe Germuska <joe@germuska.com>
2020-06-16 12:24:23 -0500

    serve icon fonts correctly for WDS

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './src/template',
+        contentBase: ['./src/template', './src/css'],
+        contentBasePublicPath: ['/', '/css']
     },
 })
