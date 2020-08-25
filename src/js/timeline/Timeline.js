2bf8465183117a2bf2f66cfb8d20a324dee27b08
Joe Germuska <joe@germuska.com>
2020-08-25 13:11:37 -0500

    wait for document content loaded; add soundcite support.

--- src/js/timeline/Timeline.js
+++ src/js/timeline/Timeline.js
@@ -328,7 +328,11 @@ class Timeline {
         this._validateOptions();
         if (this.config.isValid()) {
             try {
-                this._onDataLoaded();
+                if (document.readyState === 'loading') { // Loading hasn't finished yet
+                    document.addEventListener('DOMContentLoaded', this._onDataLoaded);
+                } else {
+                    this._onDataLoaded();
+                }
             } catch (e) {
                 this.showMessage("<strong>" + this._('error') + ":</strong> " + this._translateError(e));
             }
