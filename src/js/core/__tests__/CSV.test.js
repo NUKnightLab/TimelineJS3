4d94c1c2fadcff520bbbbe0049bbbfdea24466bf
Joe Germuska <joe@germuska.com>
2020-09-28 14:55:38 -0500

    update test to catch up with b53833d3434909323bd54d138e9f130f1b1412ba

--- src/js/core/__tests__/CSV.test.js
+++ src/js/core/__tests__/CSV.test.js
@@ -8,16 +8,16 @@ test("test the CSV", () => {
 
     // depends on file contents of course
     expect(rows[0]['title']).toBe('a good day')
-    expect(rows[0]['count']).toBe(5)
+    expect(rows[0]['count']).toBe('5')
     var row0_description = `let's just pile it  all in here,  commas, "quoted strings" and 
 new lines!`
 
     expect(rows[0]['description'].trim()).toBe(row0_description.trim())
     expect(rows[1]['title']).toBe('an ok day')
-    expect(rows[1]['count']).toBe(3)
+    expect(rows[1]['count']).toBe('3')
     expect(rows[1]['description']).toBe('this is less ambitious')
     expect(rows[2]['title']).toBe('a day, i guess')
-    expect(rows[2]['count']).toBe(2)
+    expect(rows[2]['count']).toBe('2')
     expect(rows[2]['description']).toBe('what else could go wrong?')
 
 })
