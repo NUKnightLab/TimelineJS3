5fcea476204188d7b2923f73ecab11cea855563a
Joe Germuska <joe@germuska.com>
2020-11-03 13:12:16 -0600

    Update Instagram media type to deal with new API. Fixes #664

--- src/js/media/types/Instagram.js
+++ src/js/media/types/Instagram.js
@@ -1,11 +1,63 @@
 import { unhtmlify, trace } from "../../core/Util";
-import { getJSON, fetchJSON } from "../../net/Net";
+import { ajax, fetchJSON } from "../../net/Net";
 import { Media } from "../Media";
 
 const CLIENT_TOKEN = '830b21071290df4f81a35c56abbea096'
 const FB_APP_ID = '704270473831239'
 const ACCESS_TOKEN = `${FB_APP_ID}|${CLIENT_TOKEN}`
 const API_URL_ROOT = `https://graph.facebook.com/v8.0/instagram_oembed?access_token=${ACCESS_TOKEN}&fields=html,thumbnail_url,author_name&url=`
+
+/**
+ * Break out from AJAX call for clarity. Remember to .bind() a this value.
+ * @param {XMLHttpResponse} resp 
+ */
+function successHandler(resp) {
+
+    this.oembed_response = resp;
+
+    // Link
+    this._el.content_link = this.domCreate("a", "", this._el.content);
+    this._el.content_link.href = this.data.url;
+    this._el.content_link.target = "_blank";
+
+    // Photo
+    this._el.content_item = this.domCreate("img", "tl-media-item tl-media-image tl-media-instagram tl-media-shadow", this._el.content_link);
+
+    if (this.data.alt) {
+        this._el.content_item.alt = this.data.alt;
+    } else if (this.data.caption) {
+        this._el.content_item.alt = unhtmlify(this.data.caption);
+    }
+
+    if (this.data.title) {
+        this._el.content_item.title = this.data.title;
+    } else if (this.data.caption) {
+        this._el.content_item.title = unhtmlify(this.data.caption);
+    }
+
+    // Media Loaded Event
+    this._el.content_item.addEventListener('load', function(e) {
+        this.onMediaLoaded();
+    }.bind(this));
+
+    this._el.content_item.src = resp.thumbnail_url
+
+    // After Loaded
+    this.onLoaded();
+
+}
+
+/**
+ * Break out from AJAX call for clarity. Remember to .bind() a this value.
+ * @param {XMLHttpResponse} resp
+ */
+function errorHandler(resp) {
+    let msg = `${resp.statusText} [${resp.status}]`
+    if (resp.status == 400) {
+        msg = this._('instagram_bad_request')
+    }
+    this.loadErrorDisplay(msg)
+}
 
 export default class Instagram extends Media {
 
@@ -25,41 +77,21 @@ export default class Instagram extends Media {
         this.oembed_response = null;
         var self = this;
 
-        getJSON(`${API_URL_ROOT}${this.data.url}`, (resp) => {
+        let data_url = `${API_URL_ROOT}${this.data.url}`
+        try {
+            ajax({ // getJSON doesn't let us set an errorhandler :-(
+                url: data_url,
+                dataType: 'json',
+                success: successHandler,
+                error: errorHandler,
+                context: this
+            })
+        } catch (e) {
+            console.log(`Instagram: error fetching ${data_url}`)
+            console.log(e)
+            debugger;
+        }
 
-            self.oembed_response = resp;
-
-            // Link
-            self._el.content_link = self.domCreate("a", "", self._el.content);
-            self._el.content_link.href = self.data.url;
-            self._el.content_link.target = "_blank";
-
-            // Photo
-            self._el.content_item = self.domCreate("img", "tl-media-item tl-media-image tl-media-instagram tl-media-shadow", self._el.content_link);
-
-            if (self.data.alt) {
-                self._el.content_item.alt = self.data.alt;
-            } else if (self.data.caption) {
-                self._el.content_item.alt = unhtmlify(self.data.caption);
-            }
-
-            if (self.data.title) {
-                self._el.content_item.title = self.data.title;
-            } else if (self.data.caption) {
-                self._el.content_item.title = unhtmlify(self.data.caption);
-            }
-
-            // Media Loaded Event
-            self._el.content_item.addEventListener('load', function(e) {
-                self.onMediaLoaded();
-            });
-
-            self._el.content_item.src = resp.thumbnail_url
-
-            // After Loaded
-            this.onLoaded();
-
-        })
 
     }
 
