98b66f837474ca47ba59376ee59d0ff9b37c6aec
Joe Germuska <joe@germuska.com>
2022-05-26 13:22:05 -0500

    more audit cleanup

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = merge.smart({
         open: true,
         allowedHosts: 'all',
         devMiddleware: {
-            stats: 'verbose'
+            stats: 'normal'
         },
         static: [{
                 directory: path.join(__dirname, "src/template"),
