fcfde3cc84aba74e986c3671323497ea8b30ae60
Joe Germuska <joe@germuska.com>
2022-05-26 12:53:47 -0500

    Upgrade webpack to 4.46.0, with cascading impacts

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,26 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = merge.smart({
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: ['./src/template', './src/css'],
-        contentBasePublicPath: ['/', '/css'],
-        stats: 'verbose',
-        openPage: "/index.html",
-        disableHostCheck: true
+        open: true,
+        allowedHosts: 'all',
+        devMiddleware: {
+            stats: 'verbose'
+        },
+        static: [{
+                directory: path.join(__dirname, "src/template"),
+                publicPath: "/"
+            },
+            {
+                directory: path.join(__dirname, "src/css"),
+                publicPath: "/css"
+            }
+        ]
     },
     module: {
         rules: [{
