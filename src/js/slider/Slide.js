fb8d46d920f2bb8884329231e2c9dff3cea12e30
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-06-01 12:39:13 +0300

    Added background alt setting only if the `url` is available

--- src/js/slider/Slide.js
+++ src/js/slider/Slide.js
@@ -267,6 +267,11 @@ export class Slide {
                     this.has.background.color_value = "#000";
                     this._el.background.style.display = "block";
                 }
+                if (this.data.background.alt) {
+                    // .role = 'img' doesn't work
+                    this._el.background.setAttribute('role', 'img');
+                    this._el.background.ariaLabel = this.data.background.alt;
+                }
             }
             if (this.data.background.color) {
                 this.has.background.color = true;
@@ -278,11 +283,6 @@ export class Slide {
             }
             if (this.data.background.text_background) {
                 this._el.container.className += ' tl-text-background';
-            }
-            if (this.data.background.alt) {
-                // .role = 'img' doesn't work
-                this._el.background.setAttribute('role', 'img');
-                this._el.background.ariaLabel = this.data.background.alt;
             }
         }
 
