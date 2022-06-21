ae0b1aa204995278b633e51b89b99281ae0cdb8c
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-06-21 16:27:36 +0300

    Added aria properties for the `TimeNav` container to properly support SR

--- src/js/timenav/TimeNav.js
+++ src/js/timenav/TimeNav.js
@@ -41,6 +41,13 @@ export class TimeNav {
         }
         this._el.container.setAttribute('tabindex', '0');
 
+        // Prevents the unwanted keyboard handling by the screen readers
+        this._el.container.setAttribute('role', 'application');
+        this._el.container.setAttribute('aria-label', 'Timeline navigation');
+        this._el.container.setAttribute('aria-description',
+            'Navigate between markers with arrow keys. Press "Home" for the first and "End" for the last markers'
+        );
+
         this.config = timeline_config;
 
         //Options
