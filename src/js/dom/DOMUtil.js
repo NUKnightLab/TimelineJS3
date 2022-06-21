15c6cca66e637428f2eb96d664d5c43238a3e5fe
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-06-21 19:05:32 +0300

    Added `isInViewport` utility

--- src/js/dom/DOMUtil.js
+++ src/js/dom/DOMUtil.js
@@ -23,3 +23,13 @@ export function removeClass(el, name) {
 		return w;
 	}).replace(/^\s+/, '');
 }
+
+export function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
