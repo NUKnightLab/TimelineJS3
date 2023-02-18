010ea6c4640985d41b91ba4f5728b8c4efbd22ac
meganchung4444 <80943637+meganchung4444@users.noreply.github.com>
2023-02-18 11:26:57 -0800

    edited calls from removeClass

--- src/js/net/Net.js
+++ src/js/net/Net.js
@@ -741,7 +741,7 @@ var Zepto = (function() {
                     names = funcArg(this, name, idx, className(this))
                 names.split(/\s+/g).forEach(function(klass) {
                     (when === undefined ? !$this.hasClass(klass) : when) ?
-                    $this.addClass(klass): $this.removeClass(klass)
+                    $this.addClass(klass): $this.classList.remove(klass)
                 })
             })
         },
