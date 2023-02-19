4f26ff28c44ba252cc8cb995c1dbfc4e244df38f
EdenilsonZetino <ezgarcia2001@gmail.com>
2023-02-19 10:34:49 -0800

    EdChanges merge edits to hasClass

--- src/js/net/Net.js
+++ src/js/net/Net.js
@@ -719,7 +719,7 @@ var Zepto = (function() {
                 var cls = className(this),
                     newName = funcArg(this, name, idx, cls)
                 newName.split(/\s+/g).forEach(function(klass) {
-                    if (!$(this).hasClass(klass)) classList.push(klass)
+                    if (!$(this).classList.contains(klass)) classList.push(klass)
                 }, this)
                 classList.length && className(this, cls + (cls ? " " : "") + classList.join(" "))
             })
@@ -740,7 +740,7 @@ var Zepto = (function() {
                 var $this = $(this),
                     names = funcArg(this, name, idx, className(this))
                 names.split(/\s+/g).forEach(function(klass) {
-                    (when === undefined ? !$this.hasClass(klass) : when) ?
+                    (when === undefined ? !$this.classList.contains(klass) : when) ?
                     $this.classList.add(klass): $this.classList.remove(klass)
                 })
             })
