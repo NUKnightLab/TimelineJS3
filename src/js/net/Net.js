48b7adacac73af385c185e602c148f4b8bd332e4
Dmagical <113726909+Dmagical@users.noreply.github.com>
2023-02-22 02:47:21 +0000

    revert of Net.js

--- src/js/net/Net.js
+++ src/js/net/Net.js
@@ -741,7 +741,7 @@ var Zepto = (function() {
                     names = funcArg(this, name, idx, className(this))
                 names.split(/\s+/g).forEach(function(klass) {
                     (when === undefined ? !$this.hasClass(klass) : when) ?
-                    $this.classList.add(klass): $this.classList.remove(klass)
+                    $this.addClass(klass): $this.removeClass(klass)
                 })
             })
         },
@@ -1648,17 +1648,14 @@ export const fetchJSON = Zepto.fetchJSON;
 /*
 Copyright (c) 2010-2012 Thomas Fuchs
 http://zeptojs.com
-
 Permission is hereby granted, free of charge, to any person obtaining a copy 
 of this software and associated documentation files (the "Software"), to deal 
 in the Software without restriction, including without limitation the rights 
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
 copies of the Software, and to permit persons to whom the Software is 
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all 
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
