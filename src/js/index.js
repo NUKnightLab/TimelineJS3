1a8cb5703a51b437de272ccc5a3b9e0016acd816
Andrés Suárez-Reina <webmaster@colservers.com>
2024-06-22 17:30:58 -0500

    no less

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
-import "../less/TL.Timeline.less"
-export { Timeline }
-from "./timeline/Timeline"
-export { parseGoogleSpreadsheetURL }
-from "./core/ConfigFactory"
-export { lookupMediaType }
-from "./media/MediaType"
+// import "../less/TL.Timeline.less"
+export { Timeline } from "./timeline/Timeline"
+export { parseGoogleSpreadsheetURL } from "./core/ConfigFactory"
+export { lookupMediaType } from "./media/MediaType"
