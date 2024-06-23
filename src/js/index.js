53564ff3e7cc89d105bb5384cb26c5f58ae6a811
Andrés Suárez-Reina <webmaster@colservers.com>
2024-06-22 19:58:07 -0500

    no less - intentaando de nuevo; debo tomar timeline.css desde assets del proyecto

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import "../less/TL.Timeline.less"
+// import "../less/TL.Timeline.less"
 export { Timeline } from "./timeline/Timeline"
 export { parseGoogleSpreadsheetURL } from "./core/ConfigFactory"
 export { lookupMediaType } from "./media/MediaType"
